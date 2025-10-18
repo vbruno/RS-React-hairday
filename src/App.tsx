2b2615b73f829789fa982e4387c211b5f2aa54df
Bruno S Velho <bruno.velho@gmail.com>
2025-10-19 08:03:19 +1030

    feat: adicionar tema e fontes personalizadas com Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 function App() {
 
   return (
-    <div className="bg-black text-white h-screen">
-      ...
+    <div className="bg-gray-900 text-white h-screen">
+      <div className="flex justify-center items-center h-screen flex-col gap-5" >
+        <p>Teste de font 1</p>
+        <p className="text-title-lg" >Teste de font title lg</p>
+        <p className="text-title-md" >Teste de font title md</p>
+        <p className="text-title-sm" >Teste de font title sm</p>
+        <p className="text-text-md" >Teste de font text md</p>
+        <p className="text-text-sm" >Teste de font text sm</p>
+      </div>
     </div>
   )
 }
