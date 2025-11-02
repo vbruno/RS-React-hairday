5e29ba4e0c0ac66f097ab6407ec302cb5b03af42
Bruno S Velho <bruno.velho@gmail.com>
2025-11-03 09:45:27 +1030

    feat: adicionar estrutura de agenda com horários e ícones no componente PageHome

--- src/Pages/PageHome.tsx
+++ src/Pages/PageHome.tsx
@@ -1,11 +1,15 @@
-import Icon from "../components/atomic/icon";
 import Text from "../components/atomic/text";
-
-import CalenderIcon from "../assets/icons/Calender.svg?react"
-import CaretDownIcon from "../assets/icons/Caret-Down.svg?react"
 import Button from "../components/atomic/button";
 import InputText from "../components/atomic/inputText";
 import InputSelectTime from "../components/atomic/inputSelectTime";
+import DatePicker from "../components/atomic/datePicker";
+import Icon from "../components/atomic/icon";
+import IconButton from "../components/atomic/iconButton";
+
+import SunHorizonIcon from "../assets/icons/Sun-Horizon.svg?react"
+import CloudSunIcon from "../assets/icons/Cloud-Sun.svg?react"
+import MoonStarsIcon from "../assets/icons/Moon-Stars.svg?react"
+import TrashIcon from "../assets/icons/Lixeira.svg?react"
 
 export default function PageHome() {
   return (
@@ -20,7 +24,6 @@ export default function PageHome() {
           rounded-xl
           flex flex-col items-center
           p-20 gap-6
-          border border-yellow
         `}>
         {/* Title */}
         <div
@@ -46,31 +49,7 @@ export default function PageHome() {
               variant={"title-md"}
               className="text-gray-200!"
             >Data</Text>
-            {/* Pseudo Componente DataPicker [Preciso transformar isso em componentes] */}
-            <div
-              className={`
-                  flex items-center justify-center
-                  p-3 gap-2
-                  border border-solid border-gray-500
-                  transition
-                  hover:border-yellow
-                  rounded-lg
-                `}
-            >
-              {/* icon */}
-              <Icon
-                className="size-5 fill-yellow"
-                svg={CalenderIcon} />
-              {/* texto */}
-              <Text
-                className="flex-1"
-                variant={"text-md"}
-              >10/01/2024</Text>
-              {/* icon chevron baixo */}
-              <Icon
-                className="size-4 fill-gray-300"
-                svg={CaretDownIcon} />
-            </div>
+            <DatePicker />
             <div>
 
             </div>
@@ -151,9 +130,109 @@ export default function PageHome() {
         className={`
           basis-full h-full
           bg-transparent
+          flex flex-col items-center
+          py-20 px-28 gap-8
           border border-yellow
         `}>
-        <Text>Agenda</Text>
+        {/* Header */}
+        <div
+          className="flex w-full justify-between"
+        >
+          <div
+            className="flex flex-col gap-2"
+          >
+            <Text
+              variant={"title-lg"}
+              className="text-gray-100!"
+            >Sua Agenda</Text>
+            <Text
+              variant={"text-sm"}
+              className="text-gray-300!"
+            >Consulte os seus cortes de cabelo agendados por dia</Text>
+          </div>
+          <DatePicker />
+        </div>
+        {/* Agenda */}
+        <div
+          className="flex flex-col w-full"
+        >
+          {/* Manha */}
+          <div
+            className="flex flex-col border border-solid border-gray-600 rounded-lg"
+          >
+            {/* Header */}
+            <div
+              className={`
+                  flex items-center justify-between px-5 py-3 gap-3
+                  border-b border-solid border-gray-600
+                `}
+            >
+              <Icon
+                className="size-5 fill-yellow-dark"
+                svg={SunHorizonIcon} />
+              <Text
+                variant={"text-sm"}
+                className="flex-1"
+              >Manhã</Text>
+              <Text>09h-12h</Text>
+            </div>
+            {/* List Clients */}
+            <div
+              className="flex flex-col p-5 gap-1"
+            >
+              {/* Client */}
+              <div
+                className="flex gap-5 py-1 justify-between items-center"
+              >
+                <Text
+                  variant={"title-md"}
+                  className="text-gray-200!"
+                >11:00</Text>
+                <Text
+                  variant={"text-md"}
+                  className="flex-1 text-gray-200!"
+                >Ryan Dorwart</Text>
+                <IconButton
+                  className=""
+                  icon={TrashIcon} />
+              </div>
+              {/* Client */}
+              <div
+                className="flex gap-5 py-1 justify-between items-center"
+              >
+                <Text
+                  variant={"title-md"}
+                  className="text-gray-200!"
+                >11:00</Text>
+                <Text
+                  variant={"text-md"}
+                  className="flex-1 text-gray-200!"
+                >Ryan Dorwart</Text>
+                <IconButton
+                  className=""
+                  icon={TrashIcon} />
+              </div>
+              {/* Client */}
+              <div
+                className="flex gap-5 py-1 justify-between items-center"
+              >
+                <Text
+                  variant={"title-md"}
+                  className="text-gray-200!"
+                >11:00</Text>
+                <Text
+                  variant={"text-md"}
+                  className="flex-1 text-gray-200!"
+                >Ryan Dorwart</Text>
+                <IconButton
+                  className=""
+                  icon={TrashIcon} />
+              </div>
+            </div>
+          </div>
+          {/* Tarde */}
+          {/* Noite */}
+        </div>
       </div>
     </div>
   )
